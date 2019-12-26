784def067ecbb62f0470d2f8a5bbf4fa49fea713
Lasse <l.dancke@outlook.com>
2019-12-26 14:02:07 +0100

    Recenter on 0, 0, 0 + Primitive shrinking

---
+++ Assets/Scripts/Runtime/Player/Shrink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EscapeReality.Player
+{
+    public class Shrink : MonoBehaviour
+    {
+#pragma warning disable CS0649
+        [SerializeField, Range(0.01f, 1f)]
+        private float factor;
+        private bool shrunk;
+#pragma warning restore CS0649
+
+        private void Awake()
+        {
+            this.shrunk = false;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                float factor = this.shrunk ? 1 : this.factor;
+                transform.localScale = factor * Vector3.one;
+                this.shrunk = !this.shrunk;
+            }
+        }
+    }
+}
