8bd3964e4c40557ff8cf3783fb5bb055f0c8b6fd
Lasse <l.dancke@outlook.com>
2020-01-07 18:57:40 +0100

    GameManager + Execution order

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EscapeReality
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager instance;
+        public static GameManager Instance { get; private set; }
+    }
+}
