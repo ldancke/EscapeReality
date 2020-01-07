9aeda241afce2941795bc935299bbf8d38e80d2e
Lasse <l.dancke@outlook.com>
2020-01-07 19:42:41 +0100

    Fix GameManager null event call + New time tracker

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace EscapeReality
 {
+    public enum GameState
+    {
+        Running,
+        Stopped
+    }
+
     public class GameManager : MonoBehaviour
     {
         private static GameManager instance;
         public static GameManager Instance { get; private set; }
+
+        private GameState gameState;
+        public GameState GameState { get; private set; }
+
+        private TimeTracker timeTracker;
+        public TimeTracker TimeTracker { get; private set; }
+
+        public event Action OnGameStart;
+        public event Action OnGameStop;
+
+        private void Awake()
+        {
+            this.timeTracker = new TimeTracker();
+        }
+
+        public void GameStart()
+        {
+            if (this.gameState == GameState.Running)
+                return;
+
+            this.gameState = GameState.Running;
+            OnGameStart?.Invoke();
+        }
+
+        public void GameStop()
+        {
+            if (this.gameState == GameState.Stopped)
+                return;
+
+            this.gameState = GameState.Stopped;
+            OnGameStop?.Invoke();
+        }
     }
 }
---
+++ Assets/Scripts/TimeTracker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EscapeReality
+{
+    public class TimeTracker
+    {
+        private bool tracking;
+        private float timestamp;
+        private float elapsed;
+
+        public TimeTracker()
+        {
+            this.tracking = false;
+            this.timestamp = 0;
+            this.elapsed = 0;
+
+            GameManager.Instance.OnGameStart += Start;
+            GameManager.Instance.OnGameStop += Stop;
+        }
+
+        private void Start()
+        {
+            this.tracking = true;
+            this.timestamp = Time.time;
+            this.elapsed = 0;
+        }
+
+        private void Stop()
+        {
+            this.tracking = false;
+        }
+
+        public float Elapsed
+        {
+            get
+            {
+                if (tracking)
+                    this.elapsed = Time.time - this.timestamp;
+                return this.elapsed;
+            }
+        }
+    }
+}
